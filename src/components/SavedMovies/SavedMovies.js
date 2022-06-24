580623fc37cf9ee699ae8b5665fd8c1051703a4d
D-Kolonovich <JleOnOJLdg@yandex.ru>
2022-06-24 11:27:08 +0300

    hotfix

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -8,9 +8,6 @@ function SavedMovies() {
                 <div className="movies__wrapper">
                     <SearchForm />
                     <MoviesCardList />
-                    <div className="movies__button-wrapper">
-                        <button className="movies__button" type="button" aria-label="Увидить больше фильмов">Ещё</button>
-                    </div>
                 </div>
             </section>
     </>
